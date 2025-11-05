29b41e66a60b7600abb2967c5fac2214de93c89b
Willyhotaku13 <willyhotaku13@gmail.com>
2025-11-04 22:12:25 -0400

    hi

---
+++ Video 10 Horas YT/Tipos de datos.py
@@ -0,0 +1,7 @@
+String_Simple = "Una Sola Linea"
+
+String_No_Simple = """Primera Linea,
+Segunda Linea,
+Tercera Linea."""
+
+print(String_No_Simple)
---
+++ Video 10 Horas YT/Variables.py
@@ -0,0 +1,10 @@
+Numero = 10
+Numero += 1
+print(Numero)
+
+Nombre = "Pedro"
+Bienvenida = "bienvenido al hotel " + Nombre + " Como puedo ayudarle?"
+print(Bienvenida)
+
+Bienvenida = f"bienvenido al hotel {Numero} Como puedo ayudarle?"
+print(Bienvenida)
