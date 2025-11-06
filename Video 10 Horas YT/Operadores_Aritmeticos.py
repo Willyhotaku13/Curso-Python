c493daac47ada5c340fd9e2b4d31a690f7a1b5fc
Willyhotaku13 <willyhotaku13@gmail.com>
2025-11-05 22:22:09 -0400

    hi

---
+++ Video 10 Horas YT/Operadores_Aritmeticos.py
@@ -0,0 +1,22 @@
+#suma y resta (+ -)
+suma = 12 + 1
+resta = 14 - 1
+
+#multiplicacion y divicion (* /)
+
+multiplicacion = 13 * 2
+divicion = 12 / 2 #devuelve un float aunque sea una divicion exacta
+
+#Potenciacion (exponente) (**)
+Potenciacion = 13 ** 3
+
+#Divicion baja, Redondea eliminando la parte decimal (//)
+divicion_baja = 12 / 3
+
+#residuo o modulo (%) devuelve el sobrante de una divicion
+residuo = 12 % 3
+
+#verificar el tipo de dato
+tipo_de_dato = type(divicion)
+
+print(tipo_de_dato)
--- Video 10 Horas YT/Variables_Simples.py
+++ Video 10 Horas YT/Variables_Simples.py
@@ -1,10 +1,18 @@
 Numero = 10
+
+#
 Numero += 1
 print(Numero)
 
+#Concatenar con +
 Nombre = "Pedro"
 Bienvenida = "bienvenido al hotel " + Nombre + " Como puedo ayudarle?"
 print(Bienvenida)
 
+#Concatenar con f-strings
 Bienvenida = f"bienvenido al hotel {Numero} Como puedo ayudarle?"
 print(Bienvenida)
+
+#Operadores de Pertenencia
+print("bienvenido" in Bienvenida)
+print("bienvenido" not in Bienvenida)
